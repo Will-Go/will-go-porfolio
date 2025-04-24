c0bf5f652afea43547f290e525bf58e2e940800e
Will-Go <wgongw138@ulacit.ed.cr>
2025-04-24 14:02:11 -0600

    fix: updatable numClamp

--- components/TextDisplayer.tsx
+++ components/TextDisplayer.tsx
@@ -1,15 +1,21 @@
 //NOTE: This component is used to display text with a "see more" button if the text is too long
 //NOTE: The text will be displayed with a maximum of 3 lines, if the text is longer than that, a "see more" button will be displayed
 import React, { useRef, useState, useEffect } from "react";
+
 //UTILS
-import { cn } from "@/utils/cn";
+import { cn } from "../utils/cn";
 
 interface TextDisplayerProps {
   text: string;
   className?: string;
+  numClamp?: number;
 }
 
-export default function TextDisplayer({ text, className }: TextDisplayerProps) {
+export default function TextDisplayer({
+  text,
+  className,
+  numClamp = 3,
+}: TextDisplayerProps) {
   const typographyRef = useRef<HTMLParagraphElement | null>(null);
 
   const [seeMore, setSeeMore] = useState(false);
@@ -28,17 +34,27 @@ export default function TextDisplayer({ text, className }: TextDisplayerProps) {
     <>
       <p
         ref={typographyRef}
-        className={cn(
-          ` ${seeMore ? "overflow-auto" : "line-clamp-3"}`,
-          className
-        )}
+        className={cn("", className)}
+        style={{
+          overflow: "hidden",
+          display: "-webkit-box",
+          WebkitBoxOrient: "vertical",
+          WebkitLineClamp: seeMore ? "none" : numClamp,
+        }}
       >
         {text}
       </p>
       {isOverflowed && (
         <button
+          type="button"
           onClick={() => setSeeMore(!seeMore)}
-          className="text-accent-300 text-xs cursor-pointer "
+          className="text-accent-500! cursor-pointer text-xs mt-1 bg-transparent border-none p-0"
+          style={{
+            background: "none",
+            border: "none",
+            padding: 0,
+            font: "inherit",
+          }}
         >
           {seeMore ? "see less..." : "see more..."}
         </button>
