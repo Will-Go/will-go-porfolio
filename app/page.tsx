84905e9b9240cd4523ceb5ab8f9137522e8227d1
Will-Go <wgongw138@ulacit.ed.cr>
2025-05-25 20:56:04 -0600

    fix: merge coflict

--- app/page.tsx
+++ app/page.tsx
@@ -11,13 +11,13 @@ import { unstable_ViewTransition as ViewTransition } from "react";
 
 export default function Home() {
   return (
-    <main className="min-h-screen w-full p-6 md:p-24 overflow-x-hidden animate-fade-in">
+    <main className="relative min-h-screen p-6 md:p-24 selection:text-black selection:bg-slate-300 overflow-x-hidden animate-fade-in">
       {/* <Presentation /> */}
-      <div className="flex flex-col justify-center items-center h-[70vh] md:flex-row gap-6 overflow-visible ">
+      <div className="flex flex-col justify-center items-center h-[70vh] md:flex-row gap-6  w-full">
         <div className="grid  gap-4 place-content-center text-center md:text-left w-1/2">
           <div>
-            <span className="text-4xl font-bold animate-fade-in with-cursor">
-              Hello, I&apos;m Wilson.
+            <span className="text-2xl lg:text-4xl font-bold animate-fade-in with-cursor">
+              Hi, I&apos;m Wilson.
             </span>
           </div>
           <div className="text-lg ">
@@ -29,16 +29,14 @@ export default function Home() {
         </div>
         <div className="relative flex place-items-center ">
           {/* Background blur */}
-          <BackgroundBlur className="h-96 w-96 !absolute" />
-          <ViewTransition name="Wilson-avatar">
-            <Image
-              src="/Wilson.png"
-              alt="A picture of Wilson"
-              width={200}
-              height={200}
-              className="rounded-full max-w-[200px] max-h-[200px] object-cover relative "
-            />
-          </ViewTransition>
+          <BackgroundBlur className="h-96 w-96" />
+          <Image
+            src="/Wilson.png"
+            alt="A picture of Wilson"
+            width={200}
+            height={200}
+            className="rounded-full max-w-[200px] max-h-[200px] object-cover relative "
+          />
         </div>
       </div>
 
