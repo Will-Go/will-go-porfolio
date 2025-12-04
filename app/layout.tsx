aabcb106644e81f6905bed19530131fd24d5c399
Will-Go <wilsongongwu1@gmail.com>
2025-12-03 19:56:58 -0600

    feat: add Speed Insights integration and update package dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import { NextIntlClientProvider } from "next-intl";
 import { getLocale } from "next-intl/server";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 // import { unstable_ViewTransition as ViewTransition } from "react";
 
 export const metadata = {
@@ -31,6 +33,7 @@ export default async function RootLayout({
       >
         <NextIntlClientProvider>{children}</NextIntlClientProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
     // </ViewTransition>
