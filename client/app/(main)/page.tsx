7955147981e7c12fe733f3b0ad7a44ba9e06df30
Will-Go <wilsongongwu1@gmail.com>
2026-06-03 23:05:41 -0600

    chore: remove the snap-start classname

--- client/app/(main)/page.tsx
+++ client/app/(main)/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
 
   return (
     <main className="relative p-4 md:p-12 lg:p-20 selection:text-black selection:bg-slate-300 overflow-x-clip animate-fade-in">
-      <section className="min-h-screen flex snap-start items-center justify-center">
+      <section className="min-h-screen flex  items-center justify-center">
         <FadeInOut distance={20} threshold={0.1}>
           <Reveal
             animationType="fadeIn"
@@ -26,19 +26,15 @@ export default async function Home() {
           </Reveal>
         </FadeInOut>
       </section>
-      <section className="snap-start min-h-screen flex items-center justify-center">
+      <section className=" min-h-screen flex items-center justify-center">
         <FadeInOut distance={20} threshold={0.1}>
-          <Reveal
-            animationType="fadeIn"
-            duration={0.8}
-            easing="easeOut"
-          >
+          <Reveal animationType="fadeIn" duration={0.8} easing="easeOut">
             <hr id="about" />
             <About />
           </Reveal>
         </FadeInOut>
       </section>
-      <section className="snap-start min-h-screen flex items-center justify-center">
+      <section className=" min-h-screen flex items-center justify-center">
         <FadeInOut distance={20} threshold={0.1}>
           <Reveal
             animationType="scale"
@@ -51,7 +47,7 @@ export default async function Home() {
           </Reveal>
         </FadeInOut>
       </section>
-      <section className="snap-start min-h-screen flex items-center justify-center">
+      <section className=" min-h-screen flex items-center justify-center">
         <FadeInOut distance={30} threshold={0.1}>
           <Reveal
             animationType="slideLeft"
@@ -64,7 +60,7 @@ export default async function Home() {
           </Reveal>
         </FadeInOut>
       </section>
-      <section className="snap-start min-h-screen flex items-center justify-center">
+      <section className=" min-h-screen flex items-center justify-center">
         <FadeInOut distance={30} threshold={0.1}>
           <Reveal
             animationType="slideRight"
@@ -77,7 +73,7 @@ export default async function Home() {
           </Reveal>
         </FadeInOut>
       </section>
-      <section className="snap-start min-h-screen flex items-center justify-center">
+      <section className=" min-h-screen flex items-center justify-center">
         <FadeInOut distance={80} threshold={0.1}>
           <Reveal
             animationType="fadeUp"
